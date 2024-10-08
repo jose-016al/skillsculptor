636e0f36b9124e3933e6c37122fa6f524702c493
jose-016al <jose.allopez@outlook.es>
2024-10-08 20:16:19 +0200

    Create frontend & backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,35 @@
-import styles from "./style";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import { Navbar, Business, Project1, Project2, Project3, Project_this_Website_React, Project_WebApp_Flask, Languages, Tools, CTA, Stats, Footer, Testimonials, Hero } from "./components";
+function App() {
+  const [count, setCount] = useState(0)
 
-const App = () => (
-  <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Navbar />
-        </div>
-    </div>
-
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Hero  />
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </div>
-
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Languages />
-        <Tools />
-        <Project1 />
-        <Project_this_Website_React />
-        <Project_WebApp_Flask />
-        <Project2 />
-        <Project3 />
-        <Business />
-        <CTA />
-        {/* <Stats /> */}
-        <Footer />
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
       </div>
-    </div>
-  </div>
-);
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-export default App;
+export default App
