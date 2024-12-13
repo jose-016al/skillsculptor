f84622510f260455620a9d10bf0691847dc82f48
jose-016al <jose.allopez@outlook.es>
2024-12-13 17:31:51 +0100

    finalice

--- backend/docker/php/entrypoint.sh
+++ backend/docker/php/entrypoint.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+composer install
+
+mkdir -p config/jwt
+openssl genrsa -out config/jwt/private.pem 4096
+openssl rsa -pubout -in config/jwt/private.pem -out config/jwt/public.pem
+chmod 644 config/jwt/private.pem
+chmod 644 config/jwt/public.pem
+chown -R appuser:appuser /appdata/www/config/jwt/
+
+mkdir -p public/imagenes
+mkdir -p public/imagenes/avatars
+chmod 777 public/imagenes
+chmod 777 public/imagenes/avatars
+
 # Esperar a que la base de datos esté disponible usando nc
 echo "Esperando a que MySQL esté disponible..."
 until nc -z -v -w30 db 3306; do 
